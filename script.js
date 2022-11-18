0d7f3a84ff210e1c0518540714b3ccf08e4e8194
Shincy <shincyshnz@gmail.com>
2022-11-18 12:15:04 +0530

    keyboard press is activated

--- script.js
+++ script.js
@@ -13,6 +13,31 @@ function operate(op, operand1, operand2) {
     }
 }
 
+function appendOperand(val) {
+    if (val === 'decimal') {
+        if (!displayValue.includes('.')) {
+            val = (displayValue == '' || displayValue === 0) ? '0.' : '.';
+        } else {
+            val = '';
+        }
+    }
+    displayValue += val;
+    displayText();
+}
+
+function clickOperator(op) {
+    if (displayValue != 0 && displayValue !== 'ERROR') {
+        if (operandArray.length > 0) {
+            deactivateOperator(op);
+            result();
+        }
+        operator = op;
+        activateOperator(op);
+        operandArray.push(displayValue);
+        displayValue = '';
+    }
+}
+
 //operation performed when '=' button is clicked
 function result() {
     if (displayValue && displayValue !== 'ERROR') {
@@ -28,11 +53,16 @@ function result() {
 //change the active class of button to deactivate
 function deactivateOperator(op = null) {
     operators.forEach(operator => {
-        console.log(operator.textContent);
         if (op == null) {
             operator.classList.remove('active');
         }
         if (operator.textContent === op) operator.classList.remove('active');
+    });
+}
+
+function activateOperator(op) {
+    operators.forEach(operator => {
+        if (operator.textContent === op) operator.classList.add('active');
     });
 }
 
@@ -50,6 +80,25 @@ function clearDisplayText() {
     operator = '';
 }
 
+function deleteText() {
+    displayValue = displayValue.slice(0, -1);
+    if (displayValue.charAt(displayValue.length - 1) === '.') {
+        displayValue = displayValue.slice(0, -1);
+    }
+    displayText();
+}
+
+//if keyBoard values are pressed
+function handleKeyboardInput(e) {
+    if (e.key >= 0 && e.key <= 9) appendOperand(e.key);
+    if (e.key === '.') appendOperand('.');
+    if (e.key === '=' || e.key === 'Enter') result();
+    if (e.key === 'Backspace') deleteText();
+    if (e.key === 'Escape') clearDisplayText();
+    if (e.key === '+' || e.key === '-' || e.key === '*' || e.key === '/') {
+        clickOperator(e.key);
+    }
+}
 // ----------------------------------------------------
 
 
@@ -68,33 +117,17 @@ let operator;
 
 //If clicked button is operand
 operands.forEach(operand => {
-    operand.addEventListener('click', (e) => {
-        let val = e.target.getAttribute('data-value');
-        if (val === 'decimal') {
-            if (!displayValue.includes('.')) {
-                val = (displayValue == '' || displayValue === 0) ? '0.' : '.';
-            } else {
-                val = '';
-            }
-        }
-        displayValue += val;
-        displayText();
+    operand.addEventListener('click', (event) => {
+        let val = event.target.getAttribute('data-value');
+        appendOperand(val);
     });
 });
 
 //If clicked button is operator
-operators.forEach(op => {
-    op.addEventListener('click', () => {
-        if (displayValue != 0 && displayValue !== 'ERROR') {
-            if (operandArray.length > 0) {
-                deactivateOperator(op.textContent);
-                result();
-            }
-            operator = op.textContent;
-            op.classList.add('active');
-            operandArray.push(displayValue);
-            displayValue = '';
-        }
+operators.forEach(opr => {
+    opr.addEventListener('click', (event) => {
+        let val = event.target.getAttribute('data-value');
+        clickOperator(val);
     });
 });
 
@@ -107,13 +140,7 @@ allClear.addEventListener('click', () => {
 });
 
 //If clicked button is C
-deleteBtn.addEventListener('click', () => {
-    displayValue = displayValue.slice(0, -1);
-    if (displayValue.charAt(displayValue.length - 1) === '.') {
-        displayValue = displayValue.slice(0, -1);
-    }
-    displayText();
-});
+deleteBtn.addEventListener('click', deleteText);
 
-
+document.addEventListener('keydown', handleKeyboardInput);
 
